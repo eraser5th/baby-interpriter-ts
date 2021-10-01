727a490fde4adca0697bfff8a798938398cd794a
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-02 00:14:45 +0900

    OwaOwary

--- evaluatorTypes.ts
+++ evaluatorTypes.ts
@@ -1,1 +1,16 @@
-type evaluatorError = (type: string, envrionment: )
+/* eslint-disable no-unused-vars */
+import { Environment } from './valueTypes';
+import { Statement } from './statementAssignmentTypes';
+
+type OwaOwary = {
+  result: {
+    type: string,
+    isError: boolean,
+    message: string
+  },
+  environment: Environment
+}
+
+export type EvaluatorError = (type: Statement, envrionment: Environment) => OwaOwary
+export type TypeError = (type: Statement, environment: Environment) => OwaOwary
+export type EvaluateStatements = (statements: Statement[], environment: Environment) => OwaOwary
